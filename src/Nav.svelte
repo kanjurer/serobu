0336a6d9d96181524c4e71815eedcdb22541e73a
kanjurer <kanavb9999@gmail.com>
2021-11-13 09:56:20 -0400

    added nav bar

---
+++ src/Nav.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<style>
+nav{
+    display: flex;
+}
+
+nav ul{
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+nav ul li{
+    margin: 0px 10px;
+}
+nav ul li a{
+    text-decoration: none;
+}
+nav ul li a:hover {
+    color: orange;
+}
+
+#logo{
+    width: 200px;
+}
+</style>
+
+<nav>
+    <a href="#"><img id="logo" src = "logo.png" alt="logo" /></a>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Listings</a></li>
+        <li><a href="#">About Us</a></li>
+    </ul>
+</nav>
